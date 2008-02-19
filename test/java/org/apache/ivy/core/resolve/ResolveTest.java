b662f0d3c5de7244d6e8768c258d77164f70b0a8
Scokart Gilles <gscokart@apache.org>
2008-02-19 19:23:11 +0000

    clean import
    git-svn-id: https://svn.apache.org/repos/asf/ant/ivy/core/trunk@629198 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: b70c75229f493f173b36dc7fb0b6468b2726c0ae

--- test/java/org/apache/ivy/core/resolve/ResolveTest.java
+++ test/java/org/apache/ivy/core/resolve/ResolveTest.java
@@ -73,16 +73,6 @@ name|java
 operator|.
 name|util
 operator|.
-name|HashMap
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
 name|HashSet
 import|;
 end_import
@@ -94,16 +84,6 @@ operator|.
 name|util
 operator|.
 name|List
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
-name|Map
 import|;
 end_import
 
