d270cdf4de623759550692d31a5b89259ede4841
Maarten Coene <maartenc@apache.org>
2010-01-07 23:15:30 +0000

    Don't forget to call the init() method when a new Project instance is created!
    git-svn-id: https://svn.apache.org/repos/asf/ant/ivy/core/trunk@897049 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: c443b39bd623e94277af5cc835b04ac8901f5084

--- test/java/org/apache/ivy/ant/IvyRetrieveTest.java
+++ test/java/org/apache/ivy/ant/IvyRetrieveTest.java
@@ -1339,6 +1339,11 @@ argument_list|()
 decl_stmt|;
 name|project
 operator|.
+name|init
+argument_list|()
+expr_stmt|;
+name|project
+operator|.
 name|setProperty
 argument_list|(
 literal|"ivy.settings.file"
